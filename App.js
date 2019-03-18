cf4260257f3deb0e945912faa233e9910b24298e
agnieszkaBronsTenKate <agnieszka@bronstenkate.com>
2019-03-18 15:06:09 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+import DraggableFlatList from 'react-native-draggable-flatlist'
+
+class App extends Component {
+
+  state = {
+    data1: [...Array(20)].map((d, index) => ({
+      key: `item-${index}`,
+      label: index,
+      backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
+    })),
+    data2: [...Array(20)].map((d, index) => ({
+      key: `item-${index}`,
+      label: index,
+      backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
+    }))
+
+  }
+
+  renderItem = ({ item, index, move, moveEnd, isActive }) => {
+    return (
+      <TouchableOpacity
+        style={{ 
+          height: 100, 
+          backgroundColor: isActive ? 'blue' : item.backgroundColor,
+          alignItems: 'center', 
+          justifyContent: 'center',
+          width: 300, 
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+        onLongPress={move}
+        onPressOut={moveEnd}
+      >
+        <Text style={{ 
+          fontWeight: 'bold', 
+          color: 'white',
+          fontSize: 32,
+        }}>{item.label}</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  render() {
+    return (
+      <View style={{ flex:1, flexDirection: 'row', justifyContent: 'center' }}>
+        <View style={{ flex:1 }}>
+        <DraggableFlatList
+          data={this.state.data1}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => `draggable-item-${item.key}`}
+          scrollPercent={5}
+          onMoveEnd={({ data }) => this.setState({ data1: data })}
+        />
+        </View>
+        <View style={{ flex:1 }}>
+        <DraggableFlatList   
+          data={this.state.data2}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => `draggable-item-${item.key}`}
+          scrollPercent={5}
+          onMoveEnd={({ data }) => this.setState({ data2: data })}
+        />
+        </View>
+      </View>
+    )
+  }
+}
+
+export default App;
