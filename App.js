84ed294324791d92a89bb9bf296c913a106b9d7e
agnieszkaBronsTenKate <agnieszka@bronstenkate.com>
2019-03-19 14:57:57 +0100

    working on creating many lists

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
 import DraggableFlatList from 'react-native-draggable-flatlist'
+import data from './data.json'
 
 class App extends Component {
 
   state = {
-    data1: [...Array(20)].map((d, index) => ({
-      key: `item-${index}`,
-      label: index,
-      backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
-    })),
-    data2: [...Array(20)].map((d, index) => ({
-      key: `item-${index}`,
-      label: index,
-      backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
-    }))
-
+    // data: [...Array(20)].map((d, index) => ({
+    //   key: `item-${index}`,
+    //   label: index,
+    //   backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
+    // }))
+    data: data.components[0].slides,
+    data1: data.components
   }
 
+
+  end = (data) => {
+    console.log('data: ',data);
+  }
+
+
+
   renderItem = ({ item, index, move, moveEnd, isActive }) => {
+    console.log(item);
     return (
       <TouchableOpacity
         style={{ 
           height: 100, 
-          backgroundColor: isActive ? 'blue' : item.backgroundColor,
+          backgroundColor: isActive ? 'blue' : 'teal',
           alignItems: 'center', 
-          justifyContent: 'center',
-          width: 300, 
-          marginLeft: 'auto',
-          marginRight: 'auto',
+          justifyContent: 'center' 
         }}
         onLongPress={move}
         onPressOut={moveEnd}
@@ -37,32 +39,25 @@ class App extends Component {
           fontWeight: 'bold', 
           color: 'white',
           fontSize: 32,
-        }}>{item.label}</Text>
+        }}>{item.title}</Text>
       </TouchableOpacity>
     )
   }
 
   render() {
+    console.log(this.state)
     return (
-      <View style={{ flex:1, flexDirection: 'row', justifyContent: 'center' }}>
-        <View style={{ flex:1 }}>
-        <DraggableFlatList
-          data={this.state.data1}
-          renderItem={this.renderItem}
-          keyExtractor={(item, index) => `draggable-item-${item.key}`}
-          scrollPercent={5}
-          onMoveEnd={({ data }) => this.setState({ data1: data })}
-        />
-        </View>
-        <View style={{ flex:1 }}>
-        <DraggableFlatList   
-          data={this.state.data2}
-          renderItem={this.renderItem}
-          keyExtractor={(item, index) => `draggable-item-${item.key}`}
-          scrollPercent={5}
-          onMoveEnd={({ data }) => this.setState({ data2: data })}
-        />
-        </View>
+      <View style={{ flex: 1, borderWidth: 2, borderColor: 'red'}}>
+
+            <DraggableFlatList
+            data={this.state.data}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => `draggable-item-${item.title}`}
+            scrollPercent={5}
+            // onMoveEnd={() => this.end({data})}
+            onMoveEnd={({ data }) => this.setState({ data })}
+            />
+
       </View>
     )
   }
